546986215fe48ea2ca0cbb6c36c4cc0ea5e3b244
Ahmxxad <83606426+Ahmxxad@users.noreply.github.com>
2024-11-30 20:33:53 +0400

    added comment for broken locations in switch statment

--- main/java/com/example/androidappproject/Primary.java
+++ main/java/com/example/androidappproject/Primary.java
@@ -54,19 +54,58 @@ public class Primary extends AppCompatActivity {
 
     private String getCoordinates(String location) {
         switch (location) {
+            //ones with pointers need to change or deleted
             case "M1":
-                return "25.281861,55.473142";
+                return "25°16'54.7 55°28'23.3";
             case "M2":
-                return "25.282230,55.473810";
+                return "25°16'57.9 55°28'25.7";
             case "M3":
-                return "25.283050,55.474230";
+                return "25°17'02.1 55°28'28.7";
+            case "M4":
+                return "25°17'09.5 55°28'30.6";
+            case "M5":
+                return "M5, University of Sharjah";
+            case "M6":
+                return "25°17'08.1 55°28'33.8";
+            case "M7":
+                return "M7 Collage of Sciences (Men)";
+            case "M8":
+                return "7FPG+RC Sharjah";
+            case "M9":
+                return "M9, University of Sharjah";
+            case "M10":
+                return "25°17'12.2 55°28'36.5";
+            case "M11":
+                return "University of Sharjah: M11 - Main Building/ Admin building";
+            case "M12":
+                return "Men's Central Laboratories - M12";
             case "W1":
-                return "25.271861,55.472000";
+                return "7FVJ+6HP - University City - Sharjah";
             case "W2":
-                return "25.272100,55.472500";
-            // Add all cases for M and W locations
+                return "W2 Building - University City - Sharjah";
+            case "W3":
+                return "7FRH+CWQ - University City - Sharjah";
+            case "W4":
+                return "7FQH+R42 Sharjah";
+            case "W5":
+                return "7FQH+Q83 - University City - Sharjah";
+            case "W6":
+                return "7FQH+JFR - University City - Sharjah";
+            case "W7":
+                return "College of Communication - W7";
+            case "W8":
+                return "7FQG+CRC - University City - Sharjah";
+            case "W9":
+                return "7FQG+7X Sharjah";
+            case "W10":
+                return "7FQH+65X Sharjah";
+            case "W11":
+                return "University of Sharjah: M11 - Main Building/ Admin building";
+            case "W12":
+                return "Women's Central Laboratories - W12";
             default:
                 return "";
         }
     }
+
 }
